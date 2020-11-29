932e379bb431afe4335d0d0e395556f50db7a8cf
alfarroqie <48423485+alfarroqie@users.noreply.github.com>
2020-11-29 10:34:25 +0700

    Update API get news by search

--- app/controllers/news.controller.js
+++ app/controllers/news.controller.js
@@ -66,6 +66,22 @@ exports.findOne = (req, res) => {
       });
     });
 };
+//Retrieve all News from the database or by search.
+exports.findAll= (req, res) => {
+  const title = req.query.title;
+  var condition = title ? { title: { [Op.iLike]: `%${title}%` } } : null;
+
+  News.findAll({ where: condition })
+    .then(data => {
+      res.send(data);
+    })
+    .catch(err => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while retrieving tutorials."
+      });
+    });
+};
 
 // Update a News by the id in the request
 exports.update = (req, res) => {
